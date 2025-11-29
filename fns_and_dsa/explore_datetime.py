72c6b78f26f211f5d8b970b378d8e9bdc351f85f
faozio <135945136+faozio@users.noreply.github.com>
2025-11-29 12:48:35 -0500

    explore datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-def display_current_date_time():
+def display_current_datetime():
     current_date = datetime.now()
     print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
@@ -9,6 +9,6 @@ def calculate_future_date():
     future_date = datetime.now() + timedelta(days=user_input)
     print(f"Future date: {future_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
-display_current_date_time()
+display_current_datetime()
 calculate_future_date()
     
